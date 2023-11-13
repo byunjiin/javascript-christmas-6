e3de057a9ae694059380eee34aa42362872239a5
byunjiin <jibyun5808@naver.com>
2023-11-13 19:05:02 +0900

    feat: output 기능

---
+++ src/Output/DateOutput.js
@@ -0,0 +1,3 @@
+export function dateoutput() {
+  return true;
+}
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,8 +1,40 @@
+import { Console } from '@woowacourse/mission-utils';
+import { dateoutput } from './Output/DateOutput.js';
+import { menuoutput } from './Output/MenuOutput.js';
+import { beforetotal } from './Output/BeforeTotal.js';
+import { champagne } from './Output/Champagne.js';
+import { sumlist } from './Output/SumList.js';
+import { summoney } from './Output/SumMoney.js';
+import { aftertotal } from './Output/AfterTotal.js';
+import { badge } from './Output/Badge.js';
 const OutputView = {
-  printMenu() {
-    Console.print("<주문 메뉴>");
-    // ...
+  printStart() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n');
   },
-  // ...
+  printDate() {
+    Console.print('12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n');
+  },
+  printMenu() {
+    Console.print('<주문 메뉴>\n');
+  },
+  printBeforeTotal() {
+    Console.print('<할인 전 총주문 금액>\n');
+  },
+  printChampagne() {
+    Console.print('<증정 메뉴>\n');
+  },
+  printSumList() {
+    Console.print('<혜택 내역>\n');
+  },
+  printSumMoney() {
+    Console.print('<총혜택 금액>\n');
+  },
+  printAfterTotal() {
+    Console.print('<할인 후 예상 결제 금액>\n');
+  },
+  printBadge() {
+    Console.print('<12월 이벤트 배지>\n');
+  },
 };
+
 export default OutputView;
