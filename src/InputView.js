d8f5c1fb4307e88ca857c13f7e0d2a5f5bfe9451
byunjiin <jibyun5808@naver.com>
2023-11-14 23:22:51 +0900

    feat: input 기능 수정

--- src/InputView.js
+++ src/InputView.js
@@ -9,7 +9,8 @@ const InputView = {
   // prettier-ignore
   async readMenu() {
     const input = await Console.readLineAsync('주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n');
-    return input;
+    let newinput = input.split( /[,-]/g); // 배열로 나눠짐(정규표현식사용)
+    return newinput.map(v => v.trim()); // 문자열로 만들어서 빈칸 없애기
   },
 };
 
