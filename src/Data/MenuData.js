545ae76a4fbd5d28c07379c7f3ba1eb177922bba
byunjiin <jibyun5808@naver.com>
2023-11-14 23:23:14 +0900

    feat: 데이터

---
+++ src/Data/MenuData.js
@@ -0,0 +1,24 @@
+// prettier-ignore
+export const appetizer = {
+  '양송이수프':6000,
+  '타파스':5500,
+  '시저샐러드':8000
+}
+// prettier-ignore
+export const mainMenu = {
+  '티본스테이크':55000,
+  '바비큐립':54000,
+  '해산물파스타':35000,
+  '크리스마스파스타':25000
+}
+// prettier-ignore
+export const dssert = {
+  '초코케이크':15000,
+  '아이스크림':5000
+}
+// prettier-ignore
+export const drink = {
+  '제로콜라':3000,
+  '레드와인':60000,
+  '샴페인':25000
+}
