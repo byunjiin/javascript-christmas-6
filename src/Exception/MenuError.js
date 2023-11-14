b436c307ac628f262d2684a2f226fdd31fe8283d
byunjiin <jibyun5808@naver.com>
2023-11-15 01:19:31 +0900

    feat: 기능2. 예외처리-음료 메뉴

--- src/Exception/MenuError.js
+++ src/Exception/MenuError.js
@@ -3,11 +3,13 @@ import OutputView from '../OutputView.js';
 import { appetizer, drink, dssert, mainMenu } from '../Data/MenuData.js';
 import { number } from './Number.js';
 import { onlymenu } from './OnlyMenu.js';
+import { onlydrink } from './OnlyDrink.js';
 // 기능2. 주문할 메뉴와 갯수 받아오기(에러)
 export async function menuerror() {
   const menudata = [appetizer, mainMenu, dssert, drink];
   const menu = await InputView.readMenu();
-  const found = number(menu) || onlymenu(menu, menudata);
+  const found =
+    number(menu) || onlymenu(menu, menudata) || onlydrink(menu, menudata);
   if (found) {
     try {
       throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
