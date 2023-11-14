3c499b602cbb60633924c681f3484a53fda64e7c
byunjiin <jibyun5808@naver.com>
2023-11-15 02:02:09 +0900

    feat: 기능2. 예외처리-중복메뉴

--- src/Exception/MenuError.js
+++ src/Exception/MenuError.js
@@ -4,12 +4,14 @@ import { appetizer, drink, dssert, mainMenu } from '../Data/MenuData.js';
 import { number } from './Number.js';
 import { onlymenu } from './OnlyMenu.js';
 import { onlydrink } from './OnlyDrink.js';
+import { duplicate } from './Duplicate.js';
 // 기능2. 주문할 메뉴와 갯수 받아오기(에러)
 export async function menuerror() {
   const menudata = [appetizer, mainMenu, dssert, drink];
   const menu = await InputView.readMenu();
+  // prettier-ignore
   const found =
-    number(menu) || onlymenu(menu, menudata) || onlydrink(menu, menudata);
+    number(menu) || onlymenu(menu, menudata) || onlydrink(menu, menudata) || duplicate(menu);
   if (found) {
     try {
       throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
