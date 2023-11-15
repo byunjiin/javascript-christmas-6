3fbb849999591993101fd1376356b7dabef2b005
byunjiin <jibyun5808@naver.com>
2023-11-15 19:16:20 +0900

    feat: 출력-할인 전 총주문금액

--- src/Event/BeforeTotal.js
+++ src/Event/BeforeTotal.js
@@ -1,1 +1,20 @@
-export function beforetotal() {}
+import { menudata } from '../Exception/MenuError.js';
+// 출력-할인 전 총주문 금액
+export function beforetotal(stringmenu, numbermenu) {
+  let sum = 0;
+  for (let i = 0; i < stringmenu.length; i++) {
+    if (menudata[0][stringmenu[i]] !== undefined) {
+      sum += menudata[0][stringmenu[i]] * numbermenu[i];
+    }
+    if (menudata[1][stringmenu[i]] !== undefined) {
+      sum += menudata[1][stringmenu[i]] * numbermenu[i];
+    }
+    if (menudata[2][stringmenu[i]] !== undefined) {
+      sum += menudata[2][stringmenu[i]] * numbermenu[i];
+    }
+    if (menudata[3][stringmenu[i]] !== undefined) {
+      sum += menudata[3][stringmenu[i]] * numbermenu[i];
+    }
+  }
+  return sum;
+}
